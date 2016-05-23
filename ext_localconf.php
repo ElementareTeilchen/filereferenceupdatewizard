77abb61a298ab72ff5765fa219ae2673581cd076
Franz Kugelmann <franz.kugelmann@elementare-teilchen.de>
2016-05-23 09:43:31 +0200

    initial already basically working version

---
+++ ext_localconf.php
@@ -0,0 +1,6 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+    die('Access denied.');
+}
+
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['sysext_file_images'] = 'ElementareTeilchen\\Filereferenceupdatewizard\\Updates\\TceformsUpdateWizard';
